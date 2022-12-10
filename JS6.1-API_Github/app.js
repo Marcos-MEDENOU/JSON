2b81c599648eca243bb6fc127ee7751030309ff0
Marcos MEDENOU <marcosmedenou@gmail.com>
2022-12-10 01:05:35 +0100

    github

--- JS6.1-API_Github/app.js
+++ JS6.1-API_Github/app.js
@@ -5,24 +5,24 @@ import showUser from './ui/ui.js';
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
         const username = document.querySelector('input[name="username"]').value;
-        let views=document.getElementById("result");
-        let url='https://api.github.com/users';
+           let views = document.getElementsByClassName("result");
+        let url='https://api.github.com/users/' +username;
         let reponse= fetch(url).then(response=>response.json())
-        .then(names=>{
-            names.forEach(function(element,index) {
-                if(element.login ===username){
-                    let src="dining-3.jpg";
-                    views.innerHTML=`<img src="" alt="${element.login}" >`;
-                    views.innerHTML="Login: "+element.login+ "<br>";
-
-                    views.innerHTML+="id: "+element.id+ "<br>";
-                    views.innerHTML+="node_id: "+element.node_id + "<br>";
-                }
+            .then(names => {   
+                let img=document.getElementById("img");  
+                img.setAttribute('src', names.avatar_url)
+                let h2 = document.getElementById("h2");
+                h2.innerHTML = names.login + `(${names.name})`;
+                let h3 = document.getElementById("repository");
+                h3.innerHTML = "Public Repository: " + names.public_repos
+                let link = document.createElement('a');
+                link.setAttribute("href", names.html_url);
+                let btn = document.getElementsByClassName("button");
+                btn.appendChild(link)
+       
             });
         })
     // getUser(username)
     //     .then(user => showUser(user))
     //     .catch(err => showError(new Error(err)));
-
-});
 
